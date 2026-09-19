3e43d2b1e59eb492239566e179bdd8fb98c81509
Bryan Murasira <84919967+bryan936M@users.noreply.github.com>
2026-09-19 16:55:04 +0200

    feat(staff-ui): Complete the desktop view of the header
    - Add the accounts center
    - style the navigation, settings, and account buttons
    - Remove redundant Container component

--- app/staff/page.tsx
+++ app/staff/page.tsx
@@ -1,14 +1,16 @@
-import Container from "@/components/Container";
+import { QrCode, Settings } from "lucide-react";
 
 // type Props = {};
 
 const page = () => {
   return (
-    <div>
-      {/* Header  */}
-      <div className="h-20 bg-white">
-        <Container>
-          <div className="flex space-x-3 items-center">
+    <header className="w-full bg-[#fffdf9d1]">
+      {/* Sizing wrapper */}
+      <div className="h-20 w-full max-w-[1160px] mx-auto py-3">
+        {/* Layout wrapper */}
+        <div className="flex min-w-full justify-between items-center">
+          {/* App Icon */}
+          <div className="h-16 flex space-x-3 items-center">
             <div className="bg-red-500 text-white w-10 h-10 p-2 rounded-lg rotate-170 -scale-x-100">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -35,9 +37,30 @@ const page = () => {
               <span className="text-sm text-gray-600">Queue, made simple.</span>
             </div>
           </div>
-        </Container>
+
+          {/* Accounts */}
+          <div className="h-16 flex items-center gap-5">
+            <div className="h-10 w-full bg-stone-100 border border-solid border-[#e4ded4] rounded-lg p-1">
+              <div className="w-full h-full flex justify-between items-center gap-2">
+                <span className="bg-[#fffdf9] p-1 rounded-sm text-[#24302d] font-bold">
+                  Staff
+                </span>
+                <div className="py-1 flex gap-1 items-center">
+                  <QrCode className="h-5 w-5" />
+                  <span>Customer</span>
+                </div>
+              </div>
+            </div>
+
+            <Settings className="h-10 w-10" />
+
+            <div className="h-10 w-10 p-2 rounded-full bg-[#9eaa9c] text-white flex items-center justify-center">
+              <span>AM</span>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </header>
   );
 };
 
